07dde162b116010561469eda947284dc5885efc9
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-14 17:48:10 +0530

    up

---
+++ operators.js
@@ -0,0 +1,22 @@
+var x=10;
+var y=20;
+var z = x+y;
+console.log(z)
+
+var z=y-x;
+console.log(z)
+
+var z=x*y;
+console.log(z)
+
+ var z=x/y;
+ console.log(z)
+
+ var z= x%y;
+ console.log(z)
+
+ var z= x**y;
+ console.log(z)
+ 
+ var z=x++;
+ console.log(z)
